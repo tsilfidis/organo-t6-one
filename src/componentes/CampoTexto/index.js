52dc34dc5c0fb3724601f2063c622a2647186f3b
Panagiotis Alexandro Tsilfidis <105652220+tsilfidis@users.noreply.github.com>
2024-06-11 15:18:26 -0400

    Aula 2
    Aprendemos como passar props para um componente;
    Renderizamos listas com o .map;
    Trabalhamos com elementos aninhados usando a prop children;
    Ouvimos elementos disparados pelo DOM: o onSubmit do nosso form.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,20 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
-    const placeholderModificada = `${props.placeholder}...`
+    const placeholderModificada = `${props.placeholder}...`;
+
+    let valor = "";
+
+    const aoDigitar = (evento) => {
+        valor = evento.target.value;
+        console.log(valor);
+    }
+
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={placeholderModificada} />
+            <input onChange={aoDigitar} required={props.obrigatorio} placeholder={placeholderModificada} />
         </div>
     );
 }
