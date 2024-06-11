2b5914927c729bf99faee301d04da3f1462c36d1
Panagiotis Alexandro Tsilfidis <105652220+tsilfidis@users.noreply.github.com>
2024-06-11 15:15:52 -0400

    Aula 1
    Criamos um novo projeto React;
    Criamos componentes funcionais;
    Estilizamos componentes com CSS;
    Analisamos as dependências do projeto;
    Analisamos os comandos disponíveis no package.json.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario/index.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+
+    </div>
+  );
+}
+
+export default App;
